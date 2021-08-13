a8b6568463cbc87b8d105bda563ff0f4af4a82f4
jahmed-cloud <87301691+jahmed-cloud@users.noreply.github.com>
2021-08-13 16:50:59 +0530

    Update How to find the App group and UserName.ps1

--- How to/How to find the App group and UserName.ps1
+++ How to/How to find the App group and UserName.ps1
@@ -1,1 +1,15 @@
+$upn = "upn@email.com"
+$tenants = (Get-RdsTenant).TenantName
+foreach($tenant in $tenants)
+{
+    $hostPools = (Get-RdsHostPool -TenantName $tenant).HostPoolName
+    foreach($hostPool in $hostPools)
+    {
+       $appgroups = (Get-RdsAppGroup -TenantName $tenant -HostPoolName $hostPool).AppGroupName
 
+       foreach($appgroup in $appgroups)
+       {
+        Get-RdsAppGroupUser -TenantName $tenant -HostPoolName $hostPool -AppGroupName $appgroup -UserPrincipalName $upn -ErrorAction SilentlyContinue
+       }
+    }
+}
